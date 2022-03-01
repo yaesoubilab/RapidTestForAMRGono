7b871b31e71d341faa864df0352c9c2c38a064c2
rezayas <reza.yaesoubi@yale.edu>
2022-03-01 14:48:43 -0500

    updated impact figure

--- analysis/SimulateCalibratedModel.py
+++ analysis/SimulateCalibratedModel.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     # get model settings
     sets = GonoSettings()
-    sets.update_settings(sens=0, spec=1, prob_rapid_test=0)  # base: (0, 1, 0)
+    sets.update_settings(sens=1, spec=1, prob_rapid_test=1)  # base: (0, 1, 0)
 
     simulate_calibrated_model(n_of_sims=N_OF_TRAJS_TO_USE_FOR_SIMULATION,
                               sample_seeds_by_weights=False,
