e2cc5a76a972e941bd4e448e65b7adf104f6b36e
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2022-03-04 17:46:58 -0500

    testing scenarios

--- model/ModelParameters.py
+++ model/ModelParameters.py
@@ -60,7 +60,7 @@ class Parameters(EpiParameters):
         # exponent of the probability for the emergence of resistance for a drug
         self.exponProbRes[AB.CIP.value] = Uniform(-5, -3)
         self.exponProbRes[AB.TET.value] = Uniform(-5, -3)
-        self.exponProbRes[AB.CFX.value] = Uniform(-6, -3) # Uniform(-5, -3)
+        self.exponProbRes[AB.CFX.value] = Uniform(-6, -3)  # Uniform(-5, -3)
 
         self.probSym = Uniform(0.2, 0.8)  # Constant(0.75)
         self.tToNaturalRecovery = Uniform(1/12, 5)  # Constant(4)
