d3ef54eae3daebf5cce1e71281301eb99a1f93aa
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2022-04-22 15:53:16 -0400

    base analysis added

--- analysis/PlotScenarios.py
+++ analysis/PlotScenarios.py
@@ -1,8 +1,8 @@
-from definitions import get_scenario_names
 from model.Plots import plot_scenarios
 
 
-IF_M_AVAILABLE_FOR_FIRST_TX = False
+IF_M_AVAILABLE_FOR_FIRST_TX = True
+TEST_COVERAGE = 0.75
 X_RANGE = [-0.1, 8.1]
 Y_RANGE = [-6000, 5000]
 
@@ -10,16 +10,16 @@ Y_RANGE = [-6000, 5000]
 if __name__ == "__main__":
 
     if IF_M_AVAILABLE_FOR_FIRST_TX:
-        fig_file_name = 'figures/Changing specificity-with-M.png'
+        fig_file_name = 'figures/SA-with M-coverage {}.png'.format(TEST_COVERAGE)
         csv_file_name = 'outputs-with-M/scenarios/simulated_scenarios.csv'
     else:
-        fig_file_name = 'figures/Changing specificity-no-M.png'
+        fig_file_name = 'figures/SA-no M-coverage {}.png'.format(TEST_COVERAGE)
         csv_file_name = 'outputs-no-M/scenarios/simulated_scenarios.csv'
 
     plot_scenarios(
-        scenario_names=get_scenario_names(),
         csv_file_name=csv_file_name,
         fig_file_name=fig_file_name,
+        test_coverage=TEST_COVERAGE,
         x_range=X_RANGE,
         y_range=Y_RANGE)
 
--- model/Plots.py
+++ model/Plots.py
@@ -1,9 +1,8 @@
-import numpy as np
-
 import apace.analysis.Scenarios as S
 import apace.analysis.Trajectories as A
 import apace.analysis.VisualizeScenarios as V
-from definitions import RestProfile, SympStat, REST_PROFILES, ConvertSympAndResitAndAntiBio, SIM_DURATION, ANTIBIOTICS
+from definitions import RestProfile, SympStat, REST_PROFILES, ConvertSympAndResitAndAntiBio,\
+    SPE_VALUES, SIM_DURATION, ANTIBIOTICS
 from model import Data as D
 
 A.SUBPLOT_W_SPACE = 0.25
@@ -139,34 +138,101 @@ def plot_trajectories(prev_multiplier=52, incd_multiplier=1,
                                   file_name='figures/(valid-Tx) ' + filename)
 
 
-def plot_scenarios(scenario_names, csv_file_name, fig_file_name, x_range, y_range):
+def get_rate_percentage_life(scenarios_df, scenario_name):
+    """
+    :param scenarios_df: (scenario dataframe)
+    :param scenario_name: (string) scenario name
+    :return: (annual rate of gonorrhea cases, % cases treated with CIP, TET, or CRO, lifespan of CIP, TET, or CRO)
+        all calculated after the end of warm-up and formulated as estimated and prediction interval
+    """
+    rate = scenarios_df.get_mean_interval(
+        scenario_name=scenario_name,
+        outcome_name='Rate of gonorrhea cases (average incidence after epidemic warm-up)',
+        interval_type='p', deci=0, multiplier=100000, form=',')
+    proportion = scenarios_df.get_mean_interval(
+        scenario_name=scenario_name,
+        outcome_name='Proportion of cases treated with CIP, TET, or CRO (average incidence after epidemic warm-up)',
+        interval_type='p', deci=1, form='%')
+    life = scenarios_df.get_mean_interval(
+        scenario_name=scenario_name,
+        outcome_name='Proportion of cases treated with CIP, TET, or CRO (average incidence after epidemic warm-up)',
+        interval_type='p', deci=1, multiplier=SIM_DURATION)
+
+    return rate, proportion, life
+
+
+def print_rate_percentage_life(scenarios_df, scenario_name):
+    """
+       :param scenarios_df: (scenario dataframe)
+       :param scenario_name: (string) scenario name
+       :return: print
+                    annual rate of gonorrhea cases,
+                    % cases treated with CIP, TET, or CRO,
+                    lifespan of CIP, TET, or CRO)
+           all calculated after the end of warm-up and formulated as estimated and confidence interval
+       """
+    rate, prop, life = get_rate_percentage_life(scenarios_df=scenarios_df, scenario_name=scenario_name)
+    print('\nScenario: ', scenario_name)
+    print('\tAnnual rate of gonorrhea cases:\t\t', rate)
+    print('\tProportion of cases treated with CIP, TET, or CRO:\t', prop)
+    print('\tEffective lifespan of CIP, TET, and CFX\t\t', life)
+
+
+def print_change_rate_percentage_life(scenarios_df, scenario_name_base, scenario_name_new):
+    """
+       :param scenarios_df: (scenario dataframe)
+       :param scenario_name_base: (string) name of the base scenario
+       :param scenario_name_new: (string) name of the new scenario
+       :return: print
+                    change in annual rate of gonorrhea cases,
+                    change in % cases treated with CIP, TET, or CRO,
+                    change in lifespan of CIP, TET, or CRO)
+           all calculated after the end of warm-up and formulated as estimated and confidence interval
+       """
+
+    rate = scenarios_df.get_relative_diff_mean_interval(
+        scenario_name_base=scenario_name_base,
+        scenario_names=scenario_name_new,
+        outcome_name='Rate of gonorrhea cases (average incidence after epidemic warm-up)',
+        deci=1, form='%')
+    life = scenarios_df.get_relative_diff_mean_interval(
+        scenario_name_base=scenario_name_base,
+        scenario_names=scenario_name_new,
+        outcome_name='Proportion of cases treated with CIP, TET, or CRO (average incidence after epidemic warm-up)',
+        deci=1, form='%')
+
+    print('\nChange due to {} compared to {}'.format(scenario_name_new, scenario_name_base))
+    print('\tAnnual rate of gonorrhea cases:\t\t', rate)
+    print('\tEffective lifespan of CIP, TET, and CFX:\t\t', life)
+
+
+def plot_scenarios(csv_file_name, fig_file_name, test_coverage, x_range, y_range, print_all_scenarios=False):
 
     # read scenarios into a dataframe
     scenarios_df = S.ScenarioDataFrame(csv_file_name=csv_file_name)
 
     # get a specific outcome from a specific scenario
-    print('\nScenario name: ', 'Rate of gonorrhea cases | Effective lifespan of CIP, TET, and CFX')
-    for name in scenario_names:
-        rate = scenarios_df.get_mean_interval(
-            scenario_name=name,
-            outcome_name='Rate of gonorrhea cases (average incidence after epidemic warm-up)',
-            interval_type='c', deci=3)
-        life = scenarios_df.get_mean_interval(
-            scenario_name=name,
-            outcome_name='Proportion of cases treated with CIP, TET, or CRO (average incidence after epidemic warm-up)',
-            interval_type='c', deci=3, multiplier=SIM_DURATION)
-        print('{}: \t{} | {}'.format(name, rate, life))
+    if print_all_scenarios:
+        print('\nScenario name: ', '\tRate of gonorrhea cases | Effective lifespan of CIP, TET, and CFX')
+        for name in scenarios_df.scenarios:
+            rate, prop, life = get_rate_percentage_life(scenarios_df=scenarios_df, scenario_name=name)
+            print('{}: \t{} | {}'.format(name, rate, life))
+
+    # baseline analysis
+    print_rate_percentage_life(scenarios_df=scenarios_df, scenario_name='Status quo (no rapid test)')
+    print_rate_percentage_life(scenarios_df=scenarios_df, scenario_name='(p=0.750, q=0.975, c=0.750)')
+
+    print_change_rate_percentage_life(scenarios_df=scenarios_df,
+                                      scenario_name_base='Status quo (no rapid test)',
+                                      scenario_name_new='(p=0.750, q=0.975, c=0.750)')
 
     # plot CEA
     S.ERROR_BAR_ALPHA = 0.2
-    # # read scenarios into a dataframe
-    # df_scenarios = S.ScenarioDataFrame(
-    #     csv_file_name='outputs/scenarios/simulated_scenarios.csv')
 
     # sets of scenarios to display on the cost-effectiveness plain
     list_of_scenario_sets = []
-    list_if_remove_base_scenario = [True] * N_BREAKS_SPECIFICITY
-    for i, spec in enumerate(np.linspace(MIN_SPE, 1, N_BREAKS_SPECIFICITY)):
+
+    for i, spec in enumerate(SPE_VALUES):
         list_of_scenario_sets.append(S.SetOfScenarios(
             name='Specificity = {:.2f}'.format(spec),
             scenario_df=scenarios_df,
@@ -174,17 +240,19 @@ def plot_scenarios(scenario_names, csv_file_name, fig_file_name, x_range, y_rang
             marker='o',
             conditions_on_variables=[
                 S.ConditionOnVariable(var_name='sensitivity', if_included_in_label=True, label_format='{:.1f}'),
-                S.ConditionOnVariable(var_name='specificity', values=[spec])],
+                S.ConditionOnVariable(var_name='specificity', values=[spec]),
+                S.ConditionOnVariable(var_name='rapid test coverage', values=[test_coverage])
+            ],
             if_find_frontier=False,
-            if_show_fitted_curve=True,
-            labels_shift_x=0.03,
+            if_show_fitted_curve=False,
+            labels_shift_x=0.02,
             labels_shift_y=0.00)
         )
 
     V.plot_sets_of_scenarios(
         list_of_scenario_sets=list_of_scenario_sets,
         name_of_base_scenario='Status quo (no rapid test)',
-        list_if_remove_base_scenario=list_if_remove_base_scenario,
+        list_if_remove_base_scenario=[True] * len(SPE_VALUES),
         effect_outcome='Proportion of cases treated with CIP, TET, or CRO (average incidence after epidemic warm-up)',
         cost_outcome='Rate of gonorrhea cases (average incidence after epidemic warm-up)',
         labels=('Change in the effective lifespan of\nCIP, TET, and CRO',
