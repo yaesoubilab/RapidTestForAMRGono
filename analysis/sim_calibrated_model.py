c6bda828f5564db8ba03fd691c485973078136e0
rezayas <reza.yaesoubi@yale.edu>
2022-05-31 13:03:45 -0400

    pics

--- analysis/sim_calibrated_model.py
+++ analysis/sim_calibrated_model.py
@@ -1,7 +1,7 @@
 from model.model_settings import GonoSettings
 from model.support import simulate_calibrated_model
 
-N_OF_TRAJS = 16   # number of trajectories to simulate
+N_OF_TRAJS = 160   # number of trajectories to simulate
 
 
 def simulate_calibrated(sen=0.0, spe=1.0, coverage=0.0, if_m_available=True):
