9bc427708a60f1558e035df2161bb8874f5264a2
rezayas <reza.yaesoubi@yale.edu>
2022-07-01 18:16:30 -0400

    calibrated

--- analysis/calibrate.py
+++ analysis/calibrate.py
@@ -1,5 +1,4 @@
 import apacepy.calibration as calib
-
 from deampy.in_out_functions import TextFile
 
 from definitions import get_scenario_name, ROOT_DIR
@@ -16,9 +15,9 @@ The results will be saved under analysis/outputs/with-M or analysis/outputs/no-M
 """
 
 RUN_IN_PARALLEL = True
-N_OF_CALIBRATION_ITERATIONS = 16*1    # total number of trajectories to simulate as part of calibration
-N_OF_TRAJS_TO_USE_FOR_SIMULATION = 8*1   # number of trajectories with the highest likelihood to keep
-N_OF_RESAMPLES_FOR_PARAM_ESTIMATION = 8*1  # number of parameter values to resample for parameter estimation
+N_OF_CALIBRATION_ITERATIONS = 16*10    # total number of trajectories to simulate as part of calibration
+N_OF_TRAJS_TO_USE_FOR_SIMULATION = 16*1   # number of trajectories with the highest likelihood to keep
+N_OF_RESAMPLES_FOR_PARAM_ESTIMATION = 16*1  # number of parameter values to resample for parameter estimation
 
 
 def calibrate(if_m_available, calibration_seed):
