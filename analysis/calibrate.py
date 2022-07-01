3ff208fb6c6dca3a7de563831c58073298ee769f
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2022-07-01 18:54:42 -0400

    calib stats added

--- analysis/calibrate.py
+++ analysis/calibrate.py
@@ -15,8 +15,8 @@ The results will be saved under analysis/outputs/with-M or analysis/outputs/no-M
 """
 
 RUN_IN_PARALLEL = True
-N_OF_CALIBRATION_ITERATIONS = 16*10    # total number of trajectories to simulate as part of calibration
-N_OF_TRAJS_TO_USE_FOR_SIMULATION = 16*1   # number of trajectories with the highest likelihood to keep
+N_OF_CALIBRATION_ITERATIONS = 16*100    # total number of trajectories to simulate as part of calibration
+N_OF_TRAJS_TO_USE_FOR_SIMULATION = 16*10   # number of trajectories with the highest likelihood to keep
 N_OF_RESAMPLES_FOR_PARAM_ESTIMATION = 16*1  # number of parameter values to resample for parameter estimation
 
 
@@ -85,6 +85,6 @@ if __name__ == "__main__":
         for calib_seed in (None, 1):
 
             scenario_name = get_scenario_name(if_m_available=m_available, calibration_seed=calib_seed)
-            print('\nCalibrating scenario {}:'.format(scenario_name))
+            print("\nCalibrating scenario '{}':".format(scenario_name))
 
             calibrate(if_m_available=m_available, calibration_seed=calib_seed)
