33f7daf3badb8b827cbe7b93498aaa1f74c0fd34
rezayas <reza.yaesoubi@yale.edu>
2022-08-17 13:55:43 -0400

    new results

--- analysis/calibrate.py
+++ analysis/calibrate.py
@@ -15,9 +15,9 @@ The results will be saved under analysis/outputs/with-M or analysis/outputs/no-M
 """
 
 RUN_IN_PARALLEL = True
-N_OF_CALIBRATION_ITERATIONS = 16*2    # total number of trajectories to simulate as part of calibration
-N_OF_TRAJS_TO_USE_FOR_SIMULATION = 16*1   # number of trajectories with the highest likelihood to keep
-N_OF_RESAMPLES_FOR_PARAM_ESTIMATION = 16*1  # number of parameter values to resample for parameter estimation
+N_OF_CALIBRATION_ITERATIONS = 16*2*10    # total number of trajectories to simulate as part of calibration
+N_OF_TRAJS_TO_USE_FOR_SIMULATION = 16*2   # number of trajectories with the highest likelihood to keep
+N_OF_RESAMPLES_FOR_PARAM_ESTIMATION = 16*2  # number of parameter values to resample for parameter estimation
 
 
 def calibrate(if_m_available, calibration_seed):
